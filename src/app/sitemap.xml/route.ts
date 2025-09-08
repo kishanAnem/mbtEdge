9ab27946c001acd30fb163af06bee47b432a054c
kishanAnem <kishan3034@gmail.com>
2025-09-08 15:43:03 +0530

    Implement dynamic sitemap generation in route handler

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,4 +1,7 @@
-<?xml version="1.0" encoding="UTF-8"?>
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://mbtedge.com/</loc>
@@ -24,4 +27,11 @@
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>
-</urlset>
+</urlset>`;
+
+  return new NextResponse(xml, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
